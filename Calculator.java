e59286c802f2c6d6d9a33106221a4cd9f8d1be37
Diwakar Soma <73683780+somadiwakar@users.noreply.github.com>
2022-05-16 08:26:48 +0530

    Update and rename .java to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class Calculator {
     public static void main(String[] args){
         Scanner SC = new Scanner(System.in);
         int a = SC.nextInt();
